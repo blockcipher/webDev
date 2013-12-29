6c725d1be96e900d3682e189cbf582efb5d80deb
blockcipher <93618511@qq.com>
2013-12-29 19:27:32 +0800

    测试应用blog python manage.py runserver http://127.0.0.1:8000/blog/

--- django/blog/blog/blog/views.py
+++ django/blog/blog/blog/views.py
@@ -1,1 +1,8 @@
 # Create your views here.
+from django.http import HttpResponse
+import datetime
+
+def index(req):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
--- django/blog/blog/urls.py
+++ django/blog/blog/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, include, url
-
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
-
+import blog
+import blog.views
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'blog.views.home', name='home'),
@@ -14,5 +14,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),  
-    url(r'^blog/$', 'blog.views.index'),
+    url(r'^blog/$', blog.views.index),
 )
